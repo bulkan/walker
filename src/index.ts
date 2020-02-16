f4053d98c5688ee1f183b2403214ecc03b1db08e
Bulkan Evcimen <bulkan@gmail.com>
2020-02-16 22:55:25 +1100

    line walker

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import p5 from 'p5';
 import palettes from 'nice-color-palettes/1000.json';
 
-const sketch = (p: p5) => {
-  let x = p.windowWidth / 2;
-  let y = p.windowHeight / 2;
+new p5((p: p5) => {
+  let x = p.random(0, p.windowWidth);
+  let y = p.random(0, p.windowHeight);
   let palette = p.random(palettes);
   let colorIndex = 0;
 
@@ -12,24 +12,36 @@ const sketch = (p: p5) => {
 
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
+    // p.blendMode(p.SCREEN);
     p.background("black");
     p.colorMode(p.HSB, 255);
+
+
+    document.onkeydown = function(e) {
+      if (e.metaKey && e.keyCode === 83) {
+        p.saveCanvas(`walker-(${Date.now()}`, "png")
+        return false;
+      }
+    }
   };
 
   p.draw = () => {  
-    xoff += 0.01;
+    xoff += 0.001;
+    yoff += 0.001;
 
+    const yNoise = p.random(yoff);
     const xNoise = p.map(p.noise(xoff), 0, 1, 0, 4);
-    const moveBy = p.map(p.noise(xoff), 0, 1, 1, 5);
+    const moveBy = p.map(p.noise(xoff), 0, 1, 1, 4);
 
+    const r = xNoise;
 
-    if (xNoise >= 0 && xNoise < 1) {
+    if (r >= 0 && r < 1) {
       x += moveBy;
-    } else if (xNoise >= 1 && xNoise < 2) {
+    } else if (r >= 1 && r < 2) {
       x -= moveBy;
-    } else if (xNoise >= 2 && xNoise < 3) {
+    } else if (r >= 2 && r < 3) {
       y += moveBy;
-    } else if (xNoise > 3) {
+    } else if (r > 3) {
       y -= moveBy;
     }
 
@@ -52,17 +64,20 @@ const sketch = (p: p5) => {
     colorIndex = (colorIndex + palette.length - 1) % palette.length;
 
     const color = p.color(palette[colorIndex]);
-    color.setAlpha(p.map(xNoise, 0, 1, 0, 255));
+    // color.setAlpha(100);
+    color.setAlpha(p.map(xNoise, 0, 1, 0, 50));
 
     p.stroke(color);
-    p.strokeWeight(moveBy);
-    p.point(x, y);
+    p.strokeWeight(p.map(yNoise, 0, 1, 1, 3));
+
+    // p.fill(color);
+    p.line(x, y, (x * yNoise), (y * yNoise));
+    // p.point(x, y);
+    // p.point(x + (y * yNoise), y + (x * yNoise));
   };
 
   p.windowResized = () => {
     p.resizeCanvas(p.windowWidth, p.windowHeight);
     p.background("black");
   }
-};
-
-new p5(sketch);
+});
